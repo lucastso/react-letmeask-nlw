e48be5b199d81af91b0618dd49bc8e58b885e138
lucastso <lucastassisouza@gmail.com>
2021-06-23 11:08:07 -0300

    feat: add Google authentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,21 @@
+import { useHistory } from "react-router-dom";
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 import "../styles/auth.scss";
 import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
+  const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
+
+  async function handleCreateRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+    history.push("/rooms/new");
+  }
   return (
     <div id="page-auth">
       <aside>
@@ -18,7 +29,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask logo" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
